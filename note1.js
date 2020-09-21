3323d10fe1f44554fb86f67876a527bd51fd7f3f
ansonliew <57343638+ansonliew@users.noreply.github.com>
2020-09-21 21:22:25 +0800

    Create note1.js

---
+++ note1.js
@@ -0,0 +1,73 @@
+// var a = 10;
+// var b = 3;
+// var sum = a + b;
+// console.log(sum);
+
+// var name = "anson";
+// var surname ="liew"
+// var fullname = name + " liew";
+// //console.log(fullname);
+// console.log(name);
+
+// var age = 18;
+
+// if (age < 13) {
+//    console.log("< 13")
+// }
+// else if (age < 18) {
+//   console.log("< 18")
+// }
+// else {
+//   console.log("no age")
+// }
+
+//odd and even number
+// var numBer = 11;
+
+// if (numBer % 2 == 0){
+//   console.log("The number is an even number")
+// }
+//   else {
+//     console.log("The number is an odd number")
+// }
+
+//Validator
+// var ticketPrice = 100;
+// var age = 18;
+// var disCount1 = 0.25;
+// var disCount2 = 0.5;
+
+// if(age <= 18) {
+//   console.log("For age less than 19, the ticket price is RM" + ticketPrice * disCount2)
+// }
+// else if(18 < age && age < 41) {
+//   console.log("For age 19-40, the ticket price is RM" + ticketPrice)
+// }
+// else if(40 < age && age < 66) {
+//   console.log("For age 41-65, the ticket price is RM" + ticketPrice * disCount1)
+// }
+// else if(age >= 66) {
+//   console.log("For age 66 and above, the ticket price is RM" + ticketPrice * disCount2)
+// }
+
+//for loop
+// var i = 1;
+// for(var i; i<10 ; i++){
+//   console.log(i)
+// }
+
+// var h = 10;
+// for(var h; h>0 ; h--){
+//   console.log(h)
+// }
+
+// var j = 1;
+// for(var j; j<10 ; j+=2){
+//   console.log(j)
+// }
+
+var multiPlier = 10;
+var liNe = 12;
+for(var i=1; i<liNe+1 ; i++){
+  console.log(i + " x " + multiPlier + " = " + multiPlier*i)
+}
